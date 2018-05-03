22f7db0cffa4582ddaaa07c759c1f87ac1df9e28
djserega <galyuk.sergey@gmail.com>
2018-05-03 15:59:57 +0300

    Release 1.0.0.1.
    Set icon

--- src/MethodStore/Properties/AssemblyInfo.cs
+++ src/MethodStore/Properties/AssemblyInfo.cs
@@ -7,7 +7,7 @@ using System.Windows;
 // Общие сведения об этой сборке предоставляются следующим набором
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
-[assembly: AssemblyTitle("MethodStore")]
+[assembly: AssemblyTitle("Хранилище методов")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
@@ -51,5 +51,5 @@ using System.Windows;
 // Можно задать все значения или принять номер сборки и номер редакции по умолчанию.
 // используя "*", как показано ниже:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
