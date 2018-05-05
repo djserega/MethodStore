be760ab720130cf775640cc1ad2179f40983482d
djserega <galyuk.sergey@gmail.com>
2018-05-05 12:24:14 +0300

    add class SubscriberWather
    Wath changed dir data files

---
+++ src/MethodStore/SubscriberWatcher.cs
@@ -0,0 +1,28 @@
+﻿using System.IO;
+using System.Threading;
+
+namespace MethodStore
+{
+    internal class SubscriberWatcher
+    {
+        private CallUpdateListObjectMethodsEvents _callUpdate;
+
+        internal SubscriberWatcher(CallUpdateListObjectMethodsEvents callUpdate)
+        {
+            _callUpdate = callUpdate;
+
+            FileSystemWatcher watcher = new FileSystemWatcher(new DirFile().PathDataFiles)
+            {
+                EnableRaisingEvents = true
+            };
+            watcher.Changed += Watcher_Changed;
+        }
+
+        private void Watcher_Changed(object sender, FileSystemEventArgs e)
+        {
+            Thread.Sleep(1 * 1000);
+
+            _callUpdate.EvokeUpdateList();
+        }
+    }
+}
