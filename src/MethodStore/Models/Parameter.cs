93b0fcbbf7133bc5e8bdd065b09f6b610b967913
dj_serega <galyuk.sergey@gmail.com>
2018-05-06 20:36:06 +0300

    add properties Parameter

--- src/MethodStore/Models/Parameter.cs
+++ src/MethodStore/Models/Parameter.cs
@@ -1,10 +1,14 @@
-﻿namespace MethodStore
+﻿using System.ComponentModel;
+
+namespace MethodStore
 {
     public class Parameter
     {
         public string Name { get; set; }
         public string Type { get; set; }
         public string Description { get; set; }
+        public string ValueByDefault { get; set; }
+        public bool ByValue { get; set; }
 
         public Parameter()
         {
