a9ea37570680c43c30e8d695375a3f3cc163d5ae
dj_serega <galyuk.sergey@gmail.com>
2018-05-06 23:13:33 +0300

    parse module, method, parameter

--- src/MethodStore/TextParser.cs
+++ src/MethodStore/TextParser.cs
@@ -9,12 +9,15 @@ namespace MethodStore
 {
     internal class TextParser
     {
-        internal bool ParseClipboardToModuleAndMethodName(ObjectMethod objectMethod)
+        internal bool ParseClipboardTextToObjectMethods(ObjectMethod objectMethod)
         {
             string textClipboard = Clipboard.GetText();
 
             if (string.IsNullOrWhiteSpace(textClipboard))
                 return false;
+
+            textClipboard = textClipboard.TrimStart("Процедура ".ToCharArray());
+            textClipboard = textClipboard.TrimStart("Функция ".ToCharArray());
 
             int countDot = textClipboard.Count(f => f == '.');
             if (countDot == 1)
@@ -26,40 +29,101 @@ namespace MethodStore
                 textClipboard = textClipboard.TrimStart(
                     (objectMethod.Module + ".").ToCharArray());
 
-                int countOpeningBracket = textClipboard.Count(f => f == '(');
+                objectMethod.Module = RemoveSpace(objectMethod.Module);
 
-                if (countOpeningBracket == 1)
-                {
-                    int positionOpeningBracket = textClipboard.IndexOf("(");
-
-                    objectMethod.MethodName = new string(textClipboard.Take(positionOpeningBracket).ToArray());
-
-                    textClipboard = textClipboard.TrimStart(
-                        (objectMethod.MethodName + "(").ToCharArray());
-
-                    int countClosingBracket = textClipboard.Count(f => f == ')');
-
-                    if (countClosingBracket == 1)
-                    {
-                        string textParameter = textClipboard.TrimEnd(");".ToCharArray());
-
-                        List<Parameter> listParameters = new List<Parameter>();
-
-                        string[] textParameters = textParameter.Replace(" ", "").Split(',');
-                        foreach (string item in textParameters)
-                        {
-                            listParameters.Add(new Parameter(item));
-                        }
-                        objectMethod.Parameters = listParameters.ToArray();
-                    }
-                }
-                else
-                    objectMethod.MethodName = textClipboard;
+                ParseTextMethodNameAndParameters(objectMethod, textClipboard);
 
                 return true;
             }
             else
-                return false;
+            {
+                int countOpeningBracket = textClipboard.Count(f => f == '(');
+                if (countOpeningBracket == 1)
+                {
+                    ParseTextMethodNameAndParameters(objectMethod, textClipboard);
+
+                    return true;
+                }
+                else
+                    return false;
+            }
         }
+
+        private void ParseTextMethodNameAndParameters(ObjectMethod objectMethod, string textClipboard)
+        {
+            int countOpeningBracket = textClipboard.Count(f => f == '(');
+
+            if (countOpeningBracket == 1)
+            {
+                int positionOpeningBracket = textClipboard.IndexOf("(");
+
+                objectMethod.MethodName = new string(textClipboard.Take(positionOpeningBracket).ToArray()).Replace(" ", "");
+
+                textClipboard = textClipboard.TrimStart(
+                    (objectMethod.MethodName + "(").ToCharArray());
+
+                objectMethod.MethodName = RemoveSpace(objectMethod.MethodName);
+
+                int countClosingBracket = textClipboard.Count(f => f == ')');
+
+                if (countClosingBracket == 1)
+                {
+                    string textParameter = string.Empty;
+                    textParameter = textClipboard.TrimEnd(' ');
+                    textParameter = textParameter.TrimEnd("Экспорт".ToCharArray());
+                    textParameter = textParameter.TrimEnd(' ');
+                    textParameter = textParameter.TrimEnd(")".ToCharArray());
+
+                    List<Parameter> listParameters = new List<Parameter>();
+
+                    string[] textParameters = textParameter.Split(',');
+                    foreach (string item in textParameters)
+                    {
+                        if (string.IsNullOrWhiteSpace(item))
+                            continue;
+
+                        string textCurrentParameter = item;
+
+                        textCurrentParameter = textCurrentParameter.TrimStart().TrimEnd();
+
+                        Parameter newParameter = new Parameter();
+
+                        if (textCurrentParameter.StartsWith("Знач "))
+                        {
+                            newParameter.ByValue = true;
+                            textCurrentParameter = textCurrentParameter.Replace("Знач ", "");
+                        }
+
+                        int positionEquals = textCurrentParameter.IndexOf('=');
+                        if (positionEquals > 0)
+                        {
+                            newParameter.Name = textCurrentParameter.Substring(0, positionEquals);
+                            newParameter.ValueByDefault = TrimNotUserChar(
+                                textCurrentParameter.TrimStart(
+                                    newParameter.Name.ToCharArray()));
+                            newParameter.Name = RemoveSpace(newParameter.Name);
+                        }
+                        else
+                            newParameter.Name = RemoveSpace(textCurrentParameter);
+
+                        listParameters.Add(newParameter);
+                    }
+                    objectMethod.Parameters = listParameters.ToArray();
+                }
+            }
+            else
+                objectMethod.MethodName = textClipboard;
+        }
+
+        private string RemoveSpace(string text)
+        {
+            return text.Replace(" ", "");
+        }
+
+        private string TrimNotUserChar(string text)
+        {
+            return text.TrimStart().TrimStart('=').TrimStart().TrimEnd().TrimStart('"').TrimEnd('"');
+        }
+
     }
 }
